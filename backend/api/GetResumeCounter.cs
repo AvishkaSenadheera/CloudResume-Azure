a6fc30cfdc615efb74b5e13a99e7f33d469c29f1
Avishka <senadheeraavishka@gmail.com>
2023-01-05 18:29:51 +0530

    try 2 backend workflows

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -32,7 +32,7 @@ namespace Company.Function
             ILogger log)
 
         {
-            // here is where the counter gets updated
+            // here is where the counter gets updated try 2
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             
