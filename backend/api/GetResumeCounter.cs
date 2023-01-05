222e8c039034c174dadd6b45daa2427343bf9d92
Avishka <senadheeraavishka@gmail.com>
2023-01-05 22:51:10 +0530

    try7

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -32,7 +32,7 @@ namespace Company.Function
             ILogger log)
 
         {
-            // here is where the counter gets updated try 6
+            // here is where the counter gets updated try 7
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             
